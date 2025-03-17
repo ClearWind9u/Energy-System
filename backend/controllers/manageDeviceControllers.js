261f599f597f9c66a9cc3c6ed45d42150b33d09a
LangTuDaTinh <quangviet210804@gmail.com>
2025-03-17 21:30:10 +0700

    fix bug

--- backend/controllers/manageDeviceControllers.js
+++ backend/controllers/manageDeviceControllers.js
@@ -3,7 +3,7 @@ const db = require('../database/db')
 //lay danh sach thiet bi
 exports.getAllDevices = async (req,res) => {
  try {
-    const [result] = await db.query("SELECT * FROM device")
+    const [result] = await db.promise().query("SELECT * FROM device")
     return res.status(200).json(result)
  } catch (error) {
     console.error("Loi khi lay thong tin cac thiet bi:", error.message);
@@ -15,13 +15,13 @@ exports.addDevices = async (req,res) => {
    try {
     const {device_id, device_name, id_group} = req.body
         var sql = "SELECT * FROM device WHERE id = ? "
-        const [result] = await db.query(sql,[device_id])
+        const [result] = await db.promise().query(sql,[device_id])
         console.log(result);
         if(result.length!=0){
             return res.status(500).json({message: "da ton tai ID "})
         }
         var insertSql = "INSERT INTO device (id, name, max_energy, electric_consumed, time_used, id_group ) VALUES (?,?,0,0,0,?)"
-        await db.query(insertSql,[device_id, device_name, id_group])
+        await db.promise().query(insertSql,[device_id, device_name, id_group])
          return res.status(200).json({message:"da them thanh cong"})
    } catch (error) {
     console.error("Loi khi them thiet bi:", error.message);
@@ -32,14 +32,14 @@ exports.editDevices = async (req,res) => {
    try {
     const {device_id, device_new_name} = req.body
         var sql = "SELECT * FROM device WHERE id = ? "
-        const [result] = await db.query(sql,[device_id])
+        const [result] = await db.promise().query(sql,[device_id])
         console.log(result);
         if(result.length==0){
             return res.status(500).json({message: "Khong ton tai thiet bi can sua "})
         }
         var updateSql = "UPDATE device SET name = ? WHERE id = ?"
         const params = [device_new_name,device_id]
-        await db.query(updateSql,params)
+        await db.promise().query(updateSql,params)
          return res.status(200).json({message:"da sua thiet bi thanh cong"})
    } catch (error) {
     console.error("Loi khi sua thiet bi:", error.message);
