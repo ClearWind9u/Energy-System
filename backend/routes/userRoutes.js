a47e838493ceda64be9bfd74cb68d951619ee1aa
Lyquang <lythanhnhatquangthongnhat2004@gmail.com>
2025-03-27 11:11:24 +0700

    them updateuserbyID

--- backend/routes/userRoutes.js
+++ backend/routes/userRoutes.js
@@ -5,4 +5,5 @@ router.get("/get-user", userController.getUsers);
 router.post("/create-user", userController.createUser);
 router.post("/login", userController.handleLogin);
 router.get("/get-user/:id", userController.getUserById);
+router.patch("/update-user/:id", userController.updateUserById);
 module.exports = router;
